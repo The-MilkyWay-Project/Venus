305a58ae48a6769f09af2c863744d307fab063ae
Joshua walker <joshua.walker@sykescottages.co.uk>
2019-10-28 10:39:10 +0000

    npm

---
+++ components/venus.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Venus: function() {
+      return "<img src='/tracking/venus.png?tracking=on' alt='Venus' />"
+    }
+};
---
+++ test.js
@@ -0,0 +1,3 @@
+var tracking = require("./components/venus.js");
+
+tracking.Venus();
